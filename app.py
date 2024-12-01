0b55ad118791887ffb765e0320e6d06ebe3b9cb8
Fabiano Ramos <fabianosantosramos1978@gmail.com>
2024-12-01 01:14:56 -0300

    add requirements txt

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 
-data_frame = pd.read_csv('spotify.csv')
+df = pd.read_csv('spotify.csv')
 
-data_frame
+df.set_index('Track', inplace=True)
+st.line_chart(df[df['Stream']> 1000000000]['Stream'])
+
+df
